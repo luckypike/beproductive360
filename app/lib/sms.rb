fc9db805743362d0bf6facb25619c9f8ea51f7fa
Andy Braga <ab@luckypike.com>
2018-04-26 11:22:28 +0300

    Замена канала

--- app/lib/sms.rb
+++ app/lib/sms.rb
@@ -3,7 +3,7 @@ require 'addressable/uri'
 
 class Sms
   def self.message phone, message
-    uri = Addressable::URI.parse("https://gate.smsaero.ru/v2/sms/send?numbers[]=#{phone}&text=#{message}&sign=BP360&channel=INFO")
+    uri = Addressable::URI.parse("https://gate.smsaero.ru/v2/sms/send?numbers[]=#{phone}&text=#{message}&sign=BP360&channel=DIRECT")
 
     Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       request = Net::HTTP::Get.new uri
